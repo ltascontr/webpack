92459a694edb08ba8c8cd18fdd94b9870d7a6c17
Leonardo <tasconleonardo@gmail.com>
2020-08-21 22:52:15 -0500

    Primer commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+//Importando, en las llaves lo que necesito importar y despues del from desde donde
+import {saludar} from './js/componentes';
+import './styles.css';
+
+
+const nombre = 'Leonardo';
+
+saludar(nombre);
